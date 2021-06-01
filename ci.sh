4b97f2b2be14c6b44eb50bfe9c19035a6db715c2
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2021-06-01 09:40:40 -0400

    fix ci

--- ci.sh
+++ ci.sh
@@ -19,7 +19,7 @@ DOCKER_BUILDKIT=1 docker build -t "$DOCKER_IMAGE_NAME" --build-arg "UID=$(id -u)
 
 # execute tox in the docker container. don't run in parallel; conda has issues
 # when we do this (pkg cache operations are not atomic!)
-docker run --rm -v "$(pwd)":/mnt/workspace -t "$DOCKER_IMAGE_NAME" bash -c "
+docker run --rm -v "$(pwd)":/mnt/workspace -t "$DOCKER_IMAGE_NAME" bash -c '
     # commit checker
     pre-commit run --all-files
     # make commands
@@ -33,4 +33,4 @@ docker run --rm -v "$(pwd)":/mnt/workspace -t "$DOCKER_IMAGE_NAME" bash -c "
     git clean -dxff &&
     # clang tidy
     CC=clang-11 compiledb make &&
-    clang-tidy-11 -p compile_commands.json $(find . -name '*.c')"
+    clang-tidy-11 -p compile_commands.json $(find . -name "*.c")'
