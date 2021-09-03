74dd4a4d1885af32970be640ed2193aa5c5306a5
Noah Pendleton <noahp@users.noreply.github.com>
2021-09-03 11:46:50 -0400

    unique serial number for stm32f4

--- src/memfault_platform_config.h
+++ src/memfault_platform_config.h
@@ -5,8 +5,9 @@
 //! Copyright (c) Memfault, Inc.
 //! See License.txt for details
 //!
-//! Platform overrides for the default configuration settings in the memfault-firmware-sdk.
-//! Default configuration settings can be found in "memfault/config.h"
+//! Platform overrides for the default configuration settings in the
+//! memfault-firmware-sdk. Default configuration settings can be found in
+//! "memfault/config.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -21,6 +22,7 @@ extern "C" {
 // #define MEMFAULT_PLATFORM_COREDUMP_STORAGE_REGIONS_CUSTOM 1
 #define MEMFAULT_PLATFORM_COREDUMP_STORAGE_RAM_SIZE 8000
 #define MEMFAULT_COREDUMP_COLLECT_HEAP_STATS 1
+#define MEMFAULT_COREDUMP_COLLECT_LOG_REGIONS 1
 
 #ifdef __cplusplus
 }
--- src/memfault_platform_port.c
+++ src/memfault_platform_port.c
@@ -12,34 +12,47 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "third-party/stm32f407xx.h"
 
+static char device_serial[96 / 8 * 2 + sizeof("-noahp")] = {0};
+
+static void prv_init_device_serial(void) {
+  uint8_t uid[96 / 8] = {0};
+  memcpy(&uid, (uint8_t *)UID_BASE, sizeof(uid));
+
+  char uid_str[sizeof(uid) * 2 + 1];
+  for (size_t i = 0; i < sizeof(uid); i++) {
+    snprintf(uid_str + 2 * i, 3, "%02x", uid[i]);
+  }
+
+  snprintf(device_serial, sizeof(device_serial), "%s-noahp", uid_str);
+}
 
 void memfault_platform_get_device_info(sMemfaultDeviceInfo *info) {
-  // !FIXME: Populate with platform device information
-
   // IMPORTANT: All strings returned in info must be constant
   // or static as they will be used _after_ the function returns
 
   // See https://mflt.io/version-nomenclature for more context
-  *info = (sMemfaultDeviceInfo) {
-    // An ID that uniquely identifies the device in your fleet
-    // (i.e serial number, mac addr, chip id, etc)
-    // Regular expression defining valid device serials: ^[-a-zA-Z0-9_]+$
-    .device_serial = "DEMOSERIAL",
-     // A name to represent the firmware running on the MCU.
-    // (i.e "ble-fw", "main-fw", or a codename for your project)
-    .software_type = "app-fw",
-    // The version of the "software_type" currently running.
-    // "software_type" + "software_version" must uniquely represent
-    // a single binary
-    .software_version = "1.0.0",
-    // The revision of hardware for the device. This value must remain
-    // the same for a unique device.
-    // (i.e evt, dvt, pvt, or rev1, rev2, etc)
-    // Regular expression defining valid hardware versions: ^[-a-zA-Z0-9_\.\+]+$
-    .hardware_version = "dvt1",
+  *info = (sMemfaultDeviceInfo){
+      // An ID that uniquely identifies the device in your fleet
+      // (i.e serial number, mac addr, chip id, etc)
+      // Regular expression defining valid device serials: ^[-a-zA-Z0-9_]+$
+      .device_serial = device_serial,
+      // A name to represent the firmware running on the MCU.
+      // (i.e "ble-fw", "main-fw", or a codename for your project)
+      .software_type = "app-fw",
+      // The version of the "software_type" currently running.
+      // "software_type" + "software_version" must uniquely represent
+      // a single binary
+      .software_version = "1.0.0",
+      // The revision of hardware for the device. This value must remain
+      // the same for a unique device.
+      // (i.e evt, dvt, pvt, or rev1, rev2, etc)
+      // Regular expression defining valid hardware versions:
+      // ^[-a-zA-Z0-9_\.\+]+$
+      .hardware_version = "dvt1",
   };
 }
 
@@ -50,21 +63,21 @@ void memfault_platform_reboot(void) {
 
   __asm__("bkpt 99");
   NVIC_SystemReset();
-  while (1) { } // unreachable
+  while (1) {
+  } // unreachable
 }
 
 bool memfault_platform_time_get_current(sMemfaultCurrentTime *time) {
-  // !FIXME: If the device tracks real time, update 'unix_timestamp_secs' with seconds since epoch
-  // This will cause events logged by the SDK to be timestamped on the device rather than when they
-  // arrive on the server
-  *time = (sMemfaultCurrentTime) {
-    .type = kMemfaultCurrentTimeType_UnixEpochTimeSec,
-    .info = {
-      .unix_timestamp_secs = 0
-    },
+  // !FIXME: If the device tracks real time, update 'unix_timestamp_secs' with
+  // seconds since epoch This will cause events logged by the SDK to be
+  // timestamped on the device rather than when they arrive on the server
+  *time = (sMemfaultCurrentTime){
+      .type = kMemfaultCurrentTimeType_UnixEpochTimeSec,
+      .info = {.unix_timestamp_secs = 0},
   };
 
-  // !FIXME: If device does not track time, return false, else return true if time is valid
+  // !FIXME: If device does not track time, return false, else return true if
+  // time is valid
   return false;
 }
 
@@ -73,7 +86,8 @@ extern uint32_t _ebss;
 extern uint32_t _stack;
 
 
-size_t memfault_platform_sanitize_address_range(void *start_addr, size_t desired_size) {
+size_t memfault_platform_sanitize_address_range(void *start_addr,
+                                                size_t desired_size) {
   struct {
     uint32_t start_addr;
     size_t length;
@@ -87,7 +101,8 @@ size_t memfault_platform_sanitize_address_range(void *start_addr, size_t desired
   for (size_t i = 0; i < MEMFAULT_ARRAY_SIZE(s_mcu_mem_regions); i++) {
     const uint32_t lower_addr = s_mcu_mem_regions[i].start_addr;
     const uint32_t upper_addr = lower_addr + s_mcu_mem_regions[i].length;
-    if ((uint32_t)start_addr >= lower_addr && ((uint32_t)start_addr < upper_addr)) {
+    if ((uint32_t)start_addr >= lower_addr &&
+        ((uint32_t)start_addr < upper_addr)) {
       return MEMFAULT_MIN(desired_size, upper_addr - (uint32_t)start_addr);
     }
   }
@@ -124,8 +139,9 @@ int memfault_platform_boot(void) {
   // !FIXME: Add init to any platform specific ports here.
   // (This will be done in later steps in the getting started Guide)
 
+  prv_init_device_serial();
+
   memfault_build_info_dump();
-  memfault_device_info_dump();
   memfault_platform_reboot_tracking_boot();
   memfault_log_boot(s_log_buf_storage, sizeof(s_log_buf_storage));
 
@@ -137,16 +153,18 @@ int memfault_platform_boot(void) {
   memfault_reboot_tracking_collect_reset_info(evt_storage);
 
   sMemfaultMetricBootInfo boot_info = {
-    .unexpected_reboot_count = memfault_reboot_tracking_get_crash_count(),
+      .unexpected_reboot_count = memfault_reboot_tracking_get_crash_count(),
   };
   memfault_metrics_boot(evt_storage, &boot_info);
 
+  memfault_device_info_dump();
   MEMFAULT_LOG_INFO("Memfault Initialized!");
 
   return 0;
 }
 
-void memfault_platform_log(eMemfaultPlatformLogLevel level, const char *fmt, ...) {
+void memfault_platform_log(eMemfaultPlatformLogLevel level, const char *fmt,
+                           ...) {
   va_list args;
   va_start(args, fmt);
 
@@ -155,33 +173,36 @@ void memfault_platform_log(eMemfaultPlatformLogLevel level, const char *fmt, ...
 
   const char *lvl_str;
   switch (level) {
-    case kMemfaultPlatformLogLevel_Debug:
-      lvl_str = "D";
-      break;
+  case kMemfaultPlatformLogLevel_Debug:
+    lvl_str = "D";
+    break;
 
-    case kMemfaultPlatformLogLevel_Info:
-      lvl_str = "I";
-      break;
+  case kMemfaultPlatformLogLevel_Info:
+    lvl_str = "I";
+    break;
 
-    case kMemfaultPlatformLogLevel_Warning:
-      lvl_str = "W";
-      break;
+  case kMemfaultPlatformLogLevel_Warning:
+    lvl_str = "W";
+    break;
 
-    case kMemfaultPlatformLogLevel_Error:
-      lvl_str = "E";
-      break;
+  case kMemfaultPlatformLogLevel_Error:
+    lvl_str = "E";
+    break;
 
-    default:
-      return;
-      break;
+  default:
+    return;
+    break;
   }
 
   vsnprintf(log_buf, sizeof(log_buf), fmt, args);
 
   printf("[%s] MFLT: %s\n", lvl_str, log_buf);
+
+  va_end(args);
 }
 
-bool memfault_platform_metrics_timer_boot(uint32_t period_sec, MemfaultPlatformTimerCallback callback) {
+bool memfault_platform_metrics_timer_boot(
+    uint32_t period_sec, MemfaultPlatformTimerCallback callback) {
   (void)period_sec, (void)callback;
   // Schedule a timer to invoke callback() repeatedly after period_sec
   return true;
@@ -196,7 +217,7 @@ MEMFAULT_PUT_IN_SECTION(".noinit.mflt_reboot_tracking")
 static uint8_t s_reboot_tracking[MEMFAULT_REBOOT_TRACKING_REGION_SIZE];
 
 void memfault_platform_reboot_tracking_boot(void) {
-  sResetBootupInfo reset_info = { 0 };
+  sResetBootupInfo reset_info = {0};
   memfault_reboot_reason_get(&reset_info);
   memfault_reboot_tracking_boot(s_reboot_tracking, &reset_info);
 }
@@ -207,15 +228,13 @@ void memfault_reboot_reason_get(sResetBootupInfo *info) {
 
   // TODO: Convert MCU specific reboot reason to memfault enum
 
-  *info = (sResetBootupInfo) {
-    .reset_reason_reg = reset_cause,
-    .reset_reason = reset_reason,
+  *info = (sResetBootupInfo){
+      .reset_reason_reg = reset_cause,
+      .reset_reason = reset_reason,
   };
 }
 
-void user_transport_send_chunk_data(void *chunk_data,
-                                    size_t chunk_data_len) {
+void user_transport_send_chunk_data(void *chunk_data, size_t chunk_data_len) {
   (void)chunk_data, (void)chunk_data_len;
   // printf("%.*s\n", chunk_data_len, (char *)chunk_data);
 }
-
