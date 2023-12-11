dfaf282774ef693b3eb53212dd7e2007638f09d3
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2023-12-11 10:07:10 -0500

    more minimal config

--- plotit.py
+++ plotit.py
@@ -2,6 +2,8 @@
 
 import sys
 import plotly.express as px
+import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
@@ -14,11 +16,15 @@ if __name__ == "__main__":
             x.append(tag.strip())
             y1.append(int(rom.strip()))
             y2.append(int(ram.strip()))
-    # convert the date array to a numpy datetime object
+    # convert the 3 lists into a panda dataframe
+    df = pd.DataFrame(np.c_[y1, y2], columns=['rom', 'ram'])
+
     fig = px.line(
+        df,
         x=x,
-        y=[y1,y2],
-        labels={"x": "tag", "y": "size"},
+        y=["rom", "ram"],
+        # dict(ROM=y1,RAM=y2),
+        labels={"x": "tag", "y": "size in bytes"},
         title="SDK Size per Tag",
     )
     fig.write_html("size_per_tag.html", auto_open=True)
